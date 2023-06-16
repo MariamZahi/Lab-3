a502bf580c3fb1733e8bf2b883a12299d5815466
Mariam Zahid <120362767+MariamZahi@users.noreply.github.com>
2023-06-16 19:21:31 -0400

    Print which side up

--- Die.kt
+++ Die.kt
@@ -1,40 +1,47 @@
 package game
+
 import kotlin.random.Random
 
 enum class Color {
     RED,
     WHITE,
     BLUE,
-
 }
 
-enum class Sides(val value: Int){
+enum class Sides(val value: Int) {
     Three(3),
     Four(4),
     Six(6),
     Twenty(20)
-
 }
 
-class Die(private val color: Color, private val numSides:Sides ){
-   //instead of over writing in the argument above 
+class Die(private val color: Color, private val numSides: Sides) {
+    // instead of over writing in the argument above
     var sideUp: Int = 1
-    private set
-    //Don't want user to mess up entries so we specify 1
+        private set
+    // Don't want user to mess up entries so we specify 1
 
-    //intialize here and specify class
+    // intialize here and specify class
     init {
         roll()
     }
 
-     // Second constructor for calling primary 
-     constructor(): this(Color.WHITE, Sides.Six)
-     constructor(numSides: Sides): this(Color.WHITE, numSides)
+    // Second constructor for calling primary
+    constructor() : this(Color.WHITE, Sides.Six)
+    constructor(numSides: Sides) : this(Color.WHITE, numSides)
 
+ 
     fun roll() {
         println("rolling the die")
-        sideUp = Random.nextInt(1, numSides.value + 1) 
+        sideUp = Random.nextInt(1, numSides.value + 1)
     }
 
+    override fun toString(): String {
+        return "A ${color.name} die with ${numSides.value} faces and currently showing a $sideUp."
+    }
 }
 
+fun main(){
+    val die = Die (Sides.Twenty)
+    print(die.sideUp)
+}
